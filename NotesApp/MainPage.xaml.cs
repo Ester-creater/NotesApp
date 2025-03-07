5b7a8c2803afbecfab7cad770cb67d6b33b74039
Henrique <henrique.rique200611@gmail.com>
2025-03-07 20:41:04 -0300

    Add project files.

---
+++ NotesApp/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿//using System.Reflection.Metadata;
+
+namespace NotesApp
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            //Pegar o nome que a pessoa digitou
+            string nome = NomeEntry.Text;
+
+
+            //Armazenar o nome em uma variável
+            //Escrever o nome que está na variável em uma caixa de texto
+           
+
+        }
+
+      
+    }
+
+}
---
+++ NotesApp/PrincipalPage.xaml.cs
@@ -0,0 +1,19 @@
+namespace NotesApp;
+
+public partial class PrincipalPage : ContentPage
+{
+	public PrincipalPage()
+	{
+		InitializeComponent();
+	}
+
+    private void SalvarBotao_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void DeletarBotao_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
